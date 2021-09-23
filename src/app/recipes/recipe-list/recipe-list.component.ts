323fa2ac3feafca10e01b417c828db533981a4b3
Shaowei <Shaowei.Zhang@Outlook.com>
2021-09-23 19:36:22 +1000

    create recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
