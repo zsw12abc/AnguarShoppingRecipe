22f368af053b484b4fb0752a3ce8a3e49b1a0bbe
SW <Shaowei.Zhang@Outlook.com>
2021-10-02 21:03:55 +1000

    Updating the Ingredient

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -34,4 +34,9 @@ export class ShoppingListService {
     // this.ingredientsChanged.emit(this.ingredients.slice());
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
