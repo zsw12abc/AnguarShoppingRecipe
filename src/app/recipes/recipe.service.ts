3c71c359ec61f643214018d923fb922828c2fa44
SW <Shaowei.Zhang@Outlook.com>
2021-10-03 11:16:53 +1100

    submit recipe form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,22 +2,24 @@ import {Recipe} from "./recipe.model";
 import {EventEmitter, Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
   // recipeSelected = new EventEmitter<Recipe>();
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test recipe',
-      'This is a simple test',
+    new Recipe('French Fires with Meat',
+      'French Fires with Meat is best',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fires', 20)
       ]),
-    new Recipe('A Test recipe2',
-      'This is a simple test2',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    new Recipe('The Best Burger',
+      'The best burger recipes',
+      'https://img.delicious.com.au/B80fkYtW/w759-h506-cfill/del/2016/10/jamie-olivers-aussie-style-burger-37334-3.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)]
@@ -38,5 +40,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, recipe: Recipe) {
+    this.recipes[index] = recipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
