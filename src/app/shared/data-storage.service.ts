d7bba427051988cd080589572857eb9f7408c44c
SW <Shaowei.Zhang@Outlook.com>
2021-10-05 18:46:58 +1100

    fetch data from server

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../recipes/recipe.service";
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class DataStorageService {
@@ -10,9 +11,17 @@ export class DataStorageService {
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    return this.http.post('https://ng-course-recipe-book-e95cc-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', recipes)
+    return this.http.put('https://ng-course-recipe-book-e95cc-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', recipes)
       .subscribe(response => {
         console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-e95cc-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+      .subscribe(recipes  => {
+        console.log('recipes', recipes)
+        this.recipeService.setRecipes(recipes);
       });
   }
 }
