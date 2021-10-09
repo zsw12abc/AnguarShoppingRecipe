d09d18652051168fdf8aac50e0c7427498c4f25b
SW <Shaowei.Zhang@Outlook.com>
2021-10-09 11:37:18 +1100

    Add Spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -7,13 +8,37 @@ import {NgForm} from "@angular/forms";
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string;
+
+  constructor(private authService: AuthService) {
+
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signup(email, password).subscribe(resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.error = 'An Error Occurred!';
+          this.isLoading = false;
+        });
+    }
     form.reset();
   }
 }
