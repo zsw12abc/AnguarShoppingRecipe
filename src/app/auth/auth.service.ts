1c6c8e8c5421bfb12f789c359be6ed97d4095bc9
SW <Shaowei.Zhang@Outlook.com>
2021-10-10 10:24:03 +1100

    add logout function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError, tap} from "rxjs/operators";
 import {BehaviorSubject, Subject, throwError} from "rxjs";
 import {User} from "./user.module";
+import {Router} from "@angular/router";
 
 export interface AuthResponseData {
   kind: string;
@@ -20,7 +21,7 @@ export class AuthService {
   // password: testtest
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   signup(email: string, password: string) {
@@ -45,6 +46,11 @@ export class AuthService {
         // console.log(resData)
         this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
       }));
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -32,4 +32,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.userSubscription.unsubscribe();
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
