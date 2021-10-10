33ba4e6b138c1c1aab4ca955db8db0d5cd2b0ade
SW <Shaowei.Zhang@Outlook.com>
2021-10-10 11:08:45 +1100

    Add user data in LocalStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -57,6 +57,23 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000))
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationData: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationData));
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   private handleError(errorRes: HttpErrorResponse) {
