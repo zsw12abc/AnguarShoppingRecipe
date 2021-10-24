0135517dbbb4adb5e2a4b0c6ca9e2ad6e3cbb9f7
SW <Shaowei.Zhang@Outlook.com>
2021-10-24 19:27:55 +1100

    clean the code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,7 +9,7 @@ import {Store} from "@ngrx/store";
 import * as fromApp from '../store/app.reducer'
 import * as AuthActions from './store/auth.actions'
 
-export interface AuthResponseData {
+/*export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string
@@ -17,7 +17,7 @@ export interface AuthResponseData {
   expiresIn: string;
   localId: string;
   registered?: boolean;
-}
+}*/
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -27,6 +27,20 @@ export class AuthService {
   private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router, private store: Store<fromApp.AppState>) {
+  }
+
+  setLogoutTimer(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout((() => {
+      this.store.dispatch(new AuthActions.Logout());
+      // this.logout();
+    }), expirationDuration)
+  }
+
+  clearLogoutTimer() {
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+      this.tokenExpirationTimer = null;
+    }
   }
 
   /*  signup(email: string, password: string) {
@@ -51,83 +65,68 @@ export class AuthService {
           // console.log(resData)
           this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
         }));
-    }*/
-
-  logout() {
-    // this.user.next(null);
-    this.store.dispatch(new AuthActions.Logout())
-    this.router.navigate(['/auth']);
-    localStorage.removeItem('userData');
-    if (this.tokenExpirationTimer) {
-      clearTimeout(this.tokenExpirationTimer);
     }
-    this.tokenExpirationTimer = null;
-  }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-    const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000))
-    const user = new User(email, userId, token, expirationDate);
-    // this.user.next(user);
-    this.store.dispatch(new AuthActions.AuthenticateSuccess({
-      email: email, userId: userId,
-      token: token, expirationDate: expirationDate
-    }))
-    this.setLogoutTimer(expiresIn * 1000);
-    localStorage.setItem('userData', JSON.stringify(user));
-  }
-
-  autoLogin() {
-    const userData: {
-      email: string;
-      id: string;
-      _token: string;
-      _tokenExpirationData: string;
-    } = JSON.parse(localStorage.getItem('userData'));
-    if (!userData) {
-      return;
-    }
-    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationData));
-    if (loadedUser.token) {
-      // this.user.next(loadedUser);
-      this.store.dispatch(new AuthActions.AuthenticateSuccess({
-        email: loadedUser.email, userId: loadedUser.id,
-        token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationData)
-      }))
-      const expirationDuration = new Date(userData._tokenExpirationData).getTime() - new Date().getTime();
-      this.setLogoutTimer(expirationDuration);
-    }
-  }
-
-  setLogoutTimer(expirationDuration: number) {
-    this.tokenExpirationTimer = setTimeout((() => {
-      this.store.dispatch(new AuthActions.Logout());
-      // this.logout();
-    }), expirationDuration)
-  }
-
-  clearLogoutTimer() {
-    if (this.tokenExpirationTimer) {
-      clearTimeout(this.tokenExpirationTimer);
+     logout() {
+      // this.user.next(null);
+      this.store.dispatch(new AuthActions.Logout())
+      this.router.navigate(['/auth']);
+      localStorage.removeItem('userData');
+      if (this.tokenExpirationTimer) {
+        clearTimeout(this.tokenExpirationTimer);
+      }
       this.tokenExpirationTimer = null;
     }
-  }
 
-  private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = "An unknown error occurred!";
-    if (!errorRes.error || !errorRes.error.error) {
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+      const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000))
+      const user = new User(email, userId, token, expirationDate);
+      // this.user.next(user);
+      this.store.dispatch(new AuthActions.AuthenticateSuccess({
+        email: email, userId: userId,
+        token: token, expirationDate: expirationDate
+      }))
+      this.setLogoutTimer(expiresIn * 1000);
+      localStorage.setItem('userData', JSON.stringify(user));
+    }
+
+    autoLogin() {
+      const userData: {
+        email: string;
+        id: string;
+        _token: string;
+        _tokenExpirationData: string;
+      } = JSON.parse(localStorage.getItem('userData'));
+      if (!userData) {
+        return;
+      }
+      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationData));
+      if (loadedUser.token) {
+        // this.user.next(loadedUser);
+        this.store.dispatch(new AuthActions.AuthenticateSuccess({
+          email: loadedUser.email, userId: loadedUser.id,
+          token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationData)
+        }))
+        const expirationDuration = new Date(userData._tokenExpirationData).getTime() - new Date().getTime();
+        this.setLogoutTimer(expirationDuration);
+      }
+    }
+     private handleError(errorRes: HttpErrorResponse) {
+      let errorMessage = "An unknown error occurred!";
+      if (!errorRes.error || !errorRes.error.error) {
+        return throwError(errorMessage);
+      }
+      switch (errorRes.error.error.message) {
+        case "EMAIL_EXISTS":
+          errorMessage = "The email is already existed"
+          break;
+        case "EMAIL_NOT_FOUND":
+          errorMessage = "This email does not exist in System";
+          break;
+        case "INVALID_PASSWORD":
+          errorMessage = 'This password is not correct.'
+          break;
+      }
       return throwError(errorMessage);
-    }
-    switch (errorRes.error.error.message) {
-      case "EMAIL_EXISTS":
-        errorMessage = "The email is already existed"
-        break;
-      case "EMAIL_NOT_FOUND":
-        errorMessage = "This email does not exist in System";
-        break;
-      case "INVALID_PASSWORD":
-        errorMessage = 'This password is not correct.'
-        break;
-    }
-    return throwError(errorMessage);
-  }
+    }*/
 }
