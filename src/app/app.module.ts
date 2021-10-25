9b0cffa32d403170770a5e6c3935a549fd0b080d
SW <Shaowei.Zhang@Outlook.com>
2021-10-25 17:46:18 +1100

    install ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {StoreModule} from "@ngrx/store";
 import * as fromApp from './store/app.reducer'
 import {EffectsModule} from "@ngrx/effects";
 import {AuthEffects} from "./auth/store/auth.effects";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import {AuthEffects} from "./auth/store/auth.effects";
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule,
   ],
