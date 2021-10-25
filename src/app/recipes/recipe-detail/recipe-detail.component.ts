651863f6f148c2b8907cd8ee0c4119cc98f5578f
SW <Shaowei.Zhang@Outlook.com>
2021-10-25 18:40:01 +1100

    fetching recipe details

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {Store} from "@ngrx/store";
+import * as fromApp from "../../store/app.reducer";
+import {map, switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,17 +18,27 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-          this.recipe = this.recipeService.getRecipe(this.id);
-        }
-      );
+    this.route.params.pipe(map(params => {
+        return +params['id'];
+      }), switchMap((id) => {
+        this.id = id;
+        return this.store.select('recipes')
+      }),
+      map(recipesState => {
+        return recipesState.recipes.find((recipe, index) => {
+          return index === this.id;
+        })
+      })
+    ).subscribe(
+      recipe => {
+        this.recipe = recipe;
+      }
+    );
   }
 
   onAddToShoppingList() {
